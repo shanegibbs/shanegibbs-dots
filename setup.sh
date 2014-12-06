f6c09845d0022176bc41354a2bf5e29592cc0538
Shane Gibbs <shane@hands.net.nz>
2014-12-05 21:33:16 -0800

    Start on setup script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -ex
+
+DOTS=`dirname "$(readlink -f $0)"`
+
+echo $DOTS
+
+ln -fs $DOTS/gitconfig $HOME/.gitconfig
+ln -fs $DOTS/tmux.conf $HOME/.tmux.conf
+ln -fs $DOTS/vimrc $HOME/.vimrc
+ln -nfs $DOTS/vim $HOME/.vim
+ln -fs $DOTS/zshrc $HOME/.zshrc
+ln -fs $DOTS/shanegibbs.zsh-theme $HOME/.oh-my-zsh/themes/shanegibbs.zsh-theme
